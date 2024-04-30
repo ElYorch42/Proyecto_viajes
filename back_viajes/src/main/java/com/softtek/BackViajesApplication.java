25a7d0a69371c9cad8c015696846d31d1b3ed0e0
ElYorch42 <jorgesegocruz@gmail.com>
2024-04-30 11:34:14 +0200

    update

---
+++ back_viajes/src/main/java/com/softtek/BackViajesApplication.java
@@ -0,0 +1,13 @@
+package com.softtek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackViajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackViajesApplication.class, args);
+	}
+
+}
---
+++ back_viajes/src/test/java/com/softtek/BackViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softtek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
