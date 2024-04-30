12cca445a21a11cfd70d3452d4a068ae8f20be70
Carlo500 <popcarlo625@gmail.com>
2024-04-30 12:32:55 +0200

    xd

--- back_viajes/src/main/java/com/softtek/BackViajesApplication.java
+++ back_viajes/src/main/java/com/softtek/BackViajesApplication.java
@@ -9,6 +9,7 @@ public class BackViajesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackViajesApplication.class, args);
 		//comentariotest
+		//Victor la chupa
 	}
 
 }
