c3d61d650234f45d6755460b25fdfa2ebed6b6a7
victorsanchezr <srabvictor@gmail.com>
2024-05-14 16:43:52 +0200

    inicio casi

--- front_viajes/src/app/app.routes.ts
+++ front_viajes/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"inicio" , component:InicioComponent}
+];
---
+++ front_viajes/src/app/inicio/inicio.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-inicio',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.css'
+})
+export class InicioComponent {
+
+}
