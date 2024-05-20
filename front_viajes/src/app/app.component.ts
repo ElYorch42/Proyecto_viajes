bb064c94f89fe32cef193c674e81695486835129
victorsanchezr <srabvictor@gmail.com>
2024-05-20 13:53:08 +0200

    footer responsive

--- front_viajes/src/app/app.component.ts
+++ front_viajes/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
