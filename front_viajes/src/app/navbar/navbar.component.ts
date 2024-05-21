71ae7f6325b253eae251a53f796ae9607fa53b53
victorsanchezr <srabvictor@gmail.com>
2024-05-21 16:58:19 +0200

    inicio casi

--- front_viajes/src/app/navbar/navbar.component.ts
+++ front_viajes/src/app/navbar/navbar.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterOutlet,CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+
+  isCollapsed1: boolean = true;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.checkScreenWidth();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.checkScreenWidth();
+  }
+
+  private checkScreenWidth(): void {
+    this.isCollapsed1 = window.innerWidth > 768;
+  }
+
+  toggleCollapse1(): void {
+    this.isCollapsed1 = !this.isCollapsed1;
+  }
+
+
 
 }
