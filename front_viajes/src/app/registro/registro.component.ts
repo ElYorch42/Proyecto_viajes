9c68d19ad180a2eff19b86be38ca6f2493e68f10
Carlo500 <popcarlo625@gmail.com>
2024-05-24 12:55:47 +0200

    a

--- front_viajes/src/app/registro/registro.component.ts
+++ front_viajes/src/app/registro/registro.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NavbarComponent } from "../navbar/navbar.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
-  selector: 'app-registro',
-  standalone: true,
-  imports: [ReactiveFormsModule],
-  templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+    selector: 'app-registro',
+    standalone: true,
+    templateUrl: './registro.component.html',
+    styleUrl: './registro.component.css',
+    imports: [ReactiveFormsModule, NavbarComponent, FooterComponent]
 })
 export class RegistroComponent {
   registerForm: FormGroup;
