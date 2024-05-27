2b463e098d7b4dd047c4130e972fa41462c0b709
victorsanchezr <srabvictor@gmail.com>
2024-05-27 09:12:43 +0200

    login

--- front_viajes/src/app/app.routes.ts
+++ front_viajes/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { ComienzoViajeComponent } from './comienzo-viaje/comienzo-viaje.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
 
 export const routes: Routes = [
     {path:'inicio' , component:InicioComponent},
+    {path:'inicio_sesion' , component:IniciarSesionComponent},
     {path:'' , redirectTo:'/inicio', pathMatch:'full'},
-    {path:'primero' , component:ComienzoViajeComponent}
+    {path:'comienzo_viajes' , component:ComienzoViajeComponent}
+   
 ];
