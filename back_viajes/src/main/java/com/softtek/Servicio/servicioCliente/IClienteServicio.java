d07fd180c4535130dd8e97f623834865605bb292
Ángel Gallardo <angelgallardoturpin@gmail.com>
2024-05-29 09:10:48 +0200

    consulta cliente

--- back_viajes/src/main/java/com/softtek/Servicio/servicioCliente/IClienteServicio.java
+++ back_viajes/src/main/java/com/softtek/Servicio/servicioCliente/IClienteServicio.java
@@ -6,4 +6,5 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface IClienteServicio extends ICRUD<Cliente,Integer> {
     UserDetailsService userDetailsService();
+    Cliente consultaPorCorreo(String correo);
 }
